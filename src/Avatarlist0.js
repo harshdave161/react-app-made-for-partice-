a42916c0fd49848dccb7435c114af0dc07e5a092
harshdave161 <40320848+harshdave161@users.noreply.github.com>
2019-10-29 00:37:46 +0530

    Update Avatarlist0.js

--- src/Avatarlist0.js
+++ src/Avatarlist0.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Avatarlist = (props) =>{
 return(
-    <div className="avatar_style grow tc ma4 bg-light-purple dib pa3 shadow-4">
+    <div className="avatar_style grow">
     <img src={`https://joeschmoe.io/api/v1/${props.name}`} alt="Avatar"/>
     <h1>{props.name}</h1>
     <p> {props.work}</p>
